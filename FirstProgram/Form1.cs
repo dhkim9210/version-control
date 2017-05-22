df22a71e749be7346bc862596779f79ad94a5a27
[KimDongHak] <[rlaehdgkr7@naver.com]>
2017-05-23 02:05:14 +0900

    first commit 20170523

---
+++ FirstProgram/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace FirstProgram
+{
+    public partial class Form1 : Form
+    {
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        int num = 0;
+        public static string PathA = null;
+        public Form1(string a)
+        {
+            InitializeComponent();
+            PathA = a;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string filePath = PathA;
+            string fileExtension = Path.GetExtension(filePath);
+            string header = rbHeaderYes.Checked ? "Yes" : "No";
+            string connectionString = string.Empty;
+            string sheetName = string.Empty;
+            string a = "$";
+            string comboboxitem;
+            
+            switch(fileExtension)
+            {
+                case ".xls":   
+                    connectionString = string.Format(Excel03ConString, filePath,header);
+                    break;
+                case ".xlsx": 
+                    connectionString = string.Format(Excel07ConString, filePath,header);
+                    break;
+            }
+
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[num]["TABLE_NAME"].ToString();
+                    for (int i = 0; i < dtExcelSchema.Rows.Count; i++)
+                    {
+                        comboboxitem = (dtExcelSchema.Rows[i]["TABLE_NAME"].ToString()).Substring(0, dtExcelSchema.Rows[i]["TABLE_NAME"].ToString().IndexOf(a));
+                        if (comboboxitem.Substring(0, 1) == "'")
+                        {
+                            comboboxitem = (comboboxitem.Substring(1));
+                        }
+                        if (i == 0)
+                        {
+                            comboBox1.Text = comboboxitem;
+                        }
+                        comboBox1.Items.Add(comboboxitem);
+                    }
+                    con.Close();
+                }
+            }
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+                        dataGridView1.AllowUserToAddRows = false; 
+                        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                        dataGridView1.AutoResizeColumns();
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            num = comboBox1.SelectedIndex;
+            dataGridView1.Columns.Clear();
+            comboBox1.Text = comboBox1.SelectedItem.ToString();
+            string filePath = PathA;
+            string fileExtension = Path.GetExtension(filePath);
+            string header = rbHeaderYes.Checked ? "Yes" : "No";
+            string connectionString = string.Empty;
+            string sheetName = string.Empty;
+
+            switch (fileExtension)
+            {
+                case ".xls":
+                    connectionString = string.Format(Excel03ConString, filePath, header);
+                    break;
+                case ".xlsx":
+                    connectionString = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[num]["TABLE_NAME"].ToString();
+                    con.Close();
+                }
+            }
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ FirstProgram/Form1.designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FirstProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbHeaderYes = new System.Windows.Forms.RadioButton();
+            this.rbHeaderNo = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(930, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(905, 449);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Sheet Select";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(241, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(896, 399);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.rbHeaderYes);
+            this.panel2.Controls.Add(this.rbHeaderNo);
+            this.panel2.Location = new System.Drawing.Point(290, 172);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(103, 32);
+            this.panel2.TabIndex = 3;
+            // 
+            // rbHeaderYes
+            // 
+            this.rbHeaderYes.AutoSize = true;
+            this.rbHeaderYes.Checked = true;
+            this.rbHeaderYes.Location = new System.Drawing.Point(5, 7);
+            this.rbHeaderYes.Name = "rbHeaderYes";
+            this.rbHeaderYes.Size = new System.Drawing.Size(45, 16);
+            this.rbHeaderYes.TabIndex = 2;
+            this.rbHeaderYes.TabStop = true;
+            this.rbHeaderYes.Text = "Yes";
+            this.rbHeaderYes.UseVisualStyleBackColor = true;
+            // 
+            // rbHeaderNo
+            // 
+            this.rbHeaderNo.AutoSize = true;
+            this.rbHeaderNo.Location = new System.Drawing.Point(54, 7);
+            this.rbHeaderNo.Name = "rbHeaderNo";
+            this.rbHeaderNo.Size = new System.Drawing.Size(39, 16);
+            this.rbHeaderNo.TabIndex = 2;
+            this.rbHeaderNo.Text = "No";
+            this.rbHeaderNo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Header";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(4, 6);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(136, 32);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 486);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Export Excel Viewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rbHeaderYes;
+        private System.Windows.Forms.RadioButton rbHeaderNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
